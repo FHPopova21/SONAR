1c764f5b6f089703ede6f8be4b20d1d8390f3b71
FHpopova21 <FHPopova21@codingburgas.bg>
2026-02-25 09:36:51 +0200

    feat: Introduce MobileNet model and refactor the training notebook.

---
+++ src/models/mobilenet_model.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class AudioMobileNetV2(nn.Module):
+    def __init__(self, n_classes=50, pretrained=True):
+        super(AudioMobileNetV2, self).__init__()
+        
+        # 1. Зареждаме готовия MobileNetV2
+        # Използваме 'DEFAULT' (най-добрите тегла) ако pretrained=True
+        weights = models.MobileNet_V2_Weights.DEFAULT if pretrained else None
+        self.model = models.mobilenet_v2(weights=weights)
+        
+        # 2. Адаптираме входа (Защото нашите спектрограми са 1-канални)
+        # Взимаме оригиналния първи слой
+        original_first_layer = self.model.features[0][0]
+        
+        # Създаваме нов слой, който очаква 1 канал (in_channels=1)
+        new_first_layer = nn.Conv2d(
+            in_channels=1, 
+            out_channels=original_first_layer.out_channels,
+            kernel_size=original_first_layer.kernel_size,
+            stride=original_first_layer.stride,
+            padding=original_first_layer.padding,
+            bias=original_first_layer.bias
+        )
+        
+        # Копираме теглата от първия канал на оригиналния слой
+        # Така не губим това, което моделът вече е научил!
+        with torch.no_grad():
+            new_first_layer.weight[:, 0:1, :, :] = original_first_layer.weight[:, 0:1, :, :]
+            
+        # Заменяме слоя в модела
+        self.model.features[0][0] = new_first_layer
+        
+        # 3. Адаптираме изхода (Класификатора)
+        # MobileNetV2 фабрично класифицира 1000 класа (кучета, котки, коли...)
+        # Ние махаме последния слой и слагаме такъв с нашите 50 аудио класа!
+        in_features = self.model.classifier[1].in_features
+        self.model.classifier[1] = nn.Linear(in_features, n_classes)
+
+    def forward(self, x):
+        return self.model(x)
